fae48e6d0b9e7bd1cb87944339cd643474157012
jihadaziezi <jihadaziezi9@gmail.com>
2023-06-21 13:56:48 +0700

    Forex

---
+++ app/src/main/java/com/jihad/forex/RootModel.java
@@ -0,0 +1,12 @@
+package com.jihad.forex;
+
+public class RootModel
+{
+    private RatesModel ratesModel;
+
+    public RootModel()  {}
+
+    public RatesModel getRatesModel() {return ratesModel;}
+
+    public void setRatesModel(RatesModel ratesModel) {this.ratesModel = ratesModel;}
+}
